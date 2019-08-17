f9e72732239adcfb6bd71c71f10c6bc0d341b890
Shawn Holcomb <sholcmb@gmail.com>
2019-08-17 05:44:52 -0500

    finished setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,23 @@ $(document).ready(function () {
     let wrBudget = 0;
     let teBudget = 0;
     let budgetTotal;
+    let budgetRemaining;
+    let qbRemaining;
+    let rbRemaining;
+    let wrRemaining;
+    let teRemaining;
+    let qbArr = [];
+    let rbArr = [];
+    let wrArr = [];
+    let teArr = [];
+    let qbOwned;
+    let rbOwned;
+    let wrOwned;
+    let teOwned;
 
     getTotal = () => {
         budgetTotal = qbBudget + rbBudget + wrBudget + teBudget;
+
 
         $('#budgetTotal').text('$' + budgetTotal);
 
@@ -16,7 +30,6 @@ $(document).ready(function () {
         } else {
             $('#setupDiv').addClass('setup-div').removeClass('setup-div-over');
         }
-
     }
 
     getValues = () => {
@@ -25,10 +38,114 @@ $(document).ready(function () {
         wrBudget = Number($('#wrBudget').val());
         teBudget = Number($('#teBudget').val());
 
-        getTotal()
+        getTotal();
+        setBudgets();
     }
 
-    getTotal()
+    getRoster = () => {
+        qbOwned = qbArr.length;
+        rbOwned = rbArr.length;
+        wrOwned = wrArr.length;
+        teOwned = teArr.length;
+        $('#qbOwned').text(qbOwned);
+        $('#rbOwned').text(rbOwned);
+        $('#wrOwned').text(wrOwned);
+        $('#teOwned').text(teOwned);
+    }
+
+    setBudgets = () => {
+        qbRemaining = qbBudget;
+        rbRemaining = rbBudget;
+        wrRemaining = wrBudget;
+        teRemaining = teBudget;
+        $('#qbRemaining').text('$' + qbRemaining);
+        $('#rbRemaining').text('$' + rbRemaining);
+        $('#wrRemaining').text('$' + wrRemaining);
+        $('#teRemaining').text('$' + teRemaining);
+        $('#budgetRemaining').text('$' + budgetTotal);
+
+    };
+
+    updateRemainingBudget = () => {
+        let qbTotal = 0;
+        let rbTotal = 0;
+        let wrTotal = 0;
+        let teTotal = 0;
+        
+        qbArr.forEach((value) => {
+            qbTotal = qbTotal + value;
+        });
+        qbRemaining = qbBudget - qbTotal
+        $('#qbRemaining').text('$' + qbRemaining);
+
+        rbArr.forEach((value) => {
+            rbTotal = rbTotal + value;
+        });
+        rbRemaining = rbBudget - rbTotal
+        $('#rbRemaining').text('$' + rbRemaining);
+
+        wrArr.forEach((value) => {
+            wrTotal = wrTotal + value;
+        });
+        wrRemaining = wrBudget - wrTotal
+        $('#wrRemaining').text('$' + wrRemaining);
+
+        teArr.forEach((value) => {
+            teTotal = teTotal + value;
+        });
+        teRemaining = teBudget - teTotal
+        $('#teRemaining').text('$' + teRemaining);
+
+        let remainingTotal = budgetTotal - qbTotal - rbTotal - wrTotal - teTotal;
+
+        $('#budgetRemaining').text('$' + remainingTotal);
+    }
+
+    addQB = () => {
+        if ($('#qbPrice').val().length === 0) {
+            return
+        } else {
+            qbArr.push(Number($('#qbPrice').val()));
+            $('#qbPrice').val('');
+            getRoster();
+            updateRemainingBudget();
+        }
+    };
+
+    addRB = () => {
+        if ($('#rbPrice').val().length === 0) {
+            return
+        } else {
+            rbArr.push(Number($('#rbPrice').val()));
+            $('#rbPrice').val('');
+            getRoster();
+            updateRemainingBudget();
+        }
+    };
+
+    addWR = () => {
+        if ($('#wrPrice').val().length === 0) {
+            return
+        } else {
+            wrArr.push(Number($('#wrPrice').val()));
+            $('#wrPrice').val('');
+            getRoster();
+            updateRemainingBudget();
+        }
+    };
+
+    addTE = () => {
+        if ($('#tePrice').val().length === 0) {
+            return
+        } else {
+            teArr.push(Number($('#tePrice').val()));
+            $('#tePrice').val('');
+            getRoster();
+            updateRemainingBudget();
+        }
+    };
+
+    getRoster();
 
     $('#setup').click(() => {
         $('#openSetup').toggle();
@@ -36,4 +153,9 @@ $(document).ready(function () {
         $('#setupDiv').toggle();
     })
 
+    $('#draftTool').click(() => {
+        $('#openDraftTool').toggle();
+        $('#closeDraftTool').toggle();
+        $('#draftToolDiv').toggle();
+    })
 })
