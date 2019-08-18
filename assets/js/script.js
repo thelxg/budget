c4b425ae7f44e00b92cdf14e2c28306f4e1c601c
Shawn Holcomb <sholcmb@gmail.com>
2019-08-17 23:54:05 -0500

    ready for beta

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,203 +1,308 @@
-$(document).ready(function () {
+let qbBudget = 0;
+let rbBudget = 0;
+let wrBudget = 0;
+let teBudget = 0;
+let budgetTotal;
+let budgetRemaining;
+let qbRemaining;
+let rbRemaining;
+let wrRemaining;
+let teRemaining;
+let qbArr = [];
+let rbArr = [];
+let wrArr = [];
+let teArr = [];
 
-    let qbBudget = 0;
-    let rbBudget = 0;
-    let wrBudget = 0;
-    let teBudget = 0;
-    let budgetTotal;
-    let qbRemaining;
-    let rbRemaining;
-    let wrRemaining;
-    let teRemaining;
-    let qbArr = [];
-    let rbArr = [];
-    let wrArr = [];
-    let teArr = [];
-    let qbOwned;
-    let rbOwned;
-    let wrOwned;
-    let teOwned;
+// Gets budget input from user, 
+getValues = () => {
+    qbBudget = Number($('#qbBudget').val());
+    rbBudget = Number($('#rbBudget').val());
+    wrBudget = Number($('#wrBudget').val());
+    teBudget = Number($('#teBudget').val());
 
-    // Totals budget input from user
+    getTotal();
+    updateBudgets();
+}
 
-    getTotal = () => {
-        budgetTotal = qbBudget + rbBudget + wrBudget + teBudget;
+// Totals budget input from user and conditionally formats based on value
+getTotal = () => {
+    budgetTotal = qbBudget + rbBudget + wrBudget + teBudget;
 
-        $('#budgetTotal').text('$' + budgetTotal);
+    if (budgetTotal < 199) {
+        $('#budgetTotal').addClass('redText');
+    } else {
+        $('#budgetTotal').removeClass('redText');
+    };
 
-        if (budgetTotal > 199) {
-            $('#setupDiv').addClass('setup-div-over').removeClass('setup-div');
-        } else {
-            $('#setupDiv').addClass('setup-div').removeClass('setup-div-over');
-        }
+    if (budgetTotal > 199) {
+        $('#setupDiv').addClass('setup-div-over').removeClass('setup-div');
+    } else {
+        $('#setupDiv').addClass('setup-div').removeClass('setup-div-over');
     }
 
-    // Gets budget input from user, 
+    $('#budgetTotal').text('$' + budgetTotal);
+}
 
-    getValues = () => {
-        qbBudget = Number($('#qbBudget').val());
-        rbBudget = Number($('#rbBudget').val());
-        wrBudget = Number($('#wrBudget').val());
-        teBudget = Number($('#teBudget').val());
+// Calculates number of players by position and updates DOM
+getRoster = () => {
+    $('#qbOwned').text(qbArr.length);
+    $('#rbOwned').text(rbArr.length);
+    $('#wrOwned').text(wrArr.length);
+    $('#teOwned').text(teArr.length);
+}
 
-        getTotal();
-        setBudgets();
+//Updates DOM with remaining budget by position
+updateBudgets = () => {
+    let qbTotal = 0;
+    let rbTotal = 0;
+    let wrTotal = 0;
+    let teTotal = 0;
+
+    qbArr.forEach((value) => {
+        qbTotal = qbTotal + value;
+    });
+    qbRemaining = qbBudget - qbTotal
+    $('#qbRemaining').text('$' + qbRemaining);
+
+    rbArr.forEach((value) => {
+        rbTotal = rbTotal + value;
+    });
+    rbRemaining = rbBudget - rbTotal
+    $('#rbRemaining').text('$' + rbRemaining);
+
+    wrArr.forEach((value) => {
+        wrTotal = wrTotal + value;
+    });
+    wrRemaining = wrBudget - wrTotal
+    $('#wrRemaining').text('$' + wrRemaining);
+
+    teArr.forEach((value) => {
+        teTotal = teTotal + value;
+    });
+    teRemaining = teBudget - teTotal
+    $('#teRemaining').text('$' + teRemaining);
+
+    budgetRemaining = qbRemaining + rbRemaining + wrRemaining + teRemaining
+    $('#budgetRemaining').text('$' + budgetRemaining);
+
+    checkTotals();
+};
+
+// Updates formatting if remaining budget values are negative
+checkTotals = () => {
+    if (qbRemaining < 0) {
+        $('#qbRemaining').addClass('redText');
+    } else {
+        $('#qbRemaining').removeClass('redText');
     }
 
-    // Calculates number of players by position and updates DOM
-
-    getRoster = () => {
-        qbOwned = qbArr.length;
-        rbOwned = rbArr.length;
-        wrOwned = wrArr.length;
-        teOwned = teArr.length;
-        $('#qbOwned').text(qbOwned);
-        $('#rbOwned').text(rbOwned);
-        $('#wrOwned').text(wrOwned);
-        $('#teOwned').text(teOwned);
+    if (rbRemaining < 0) {
+        $('#rbRemaining').addClass('redText');
+    } else {
+        $('#rbRemaining').removeClass('redText');
     }
 
-    //Updates DOM with remaining budget by position
-    setBudgets = () => {
-        $('#qbRemaining').text('$' + qbBudget);
-        $('#rbRemaining').text('$' + rbBudget);
-        $('#wrRemaining').text('$' + wrBudget);
-        $('#teRemaining').text('$' + teBudget);
-        $('#budgetRemaining').text('$' + budgetTotal);
-    };
-
-    updateRemainingBudget = () => {
-        let qbTotal = 0;
-        let rbTotal = 0;
-        let wrTotal = 0;
-        let teTotal = 0;
-        let wrOverspend = 0;
-        let rbOverspend = 0;
-        let teSubtract = 0;
-        let qbSubtract = 0;
-
-        rbArr.forEach((value) => {
-            rbTotal = rbTotal + value;
-        });
-        rbRemaining = rbBudget - rbTotal
-        if (rbRemaining < 0) {
-            rbOverspend = Math.abs(rbRemaining);
-            rbRemaining = 0;
-            qbSubtract = Math.ceil(rbOverspend/2);
-            teSubtract = rbOverspend - qbSubtract;
-            console.log("rbOverspend: " + rbOverspend);
-            console.log("qbSubtract: " + qbSubtract);
-            console.log("teSubtract: " + teSubtract);
-        };
-
-
-
-        wrArr.forEach((value) => {
-            wrTotal = wrTotal + value;
-        });
-        wrRemaining = wrBudget - wrTotal
-        if (wrRemaining < 0) {
-            wrOverspend = Math.abs(wrRemaining);
-            wrRemaining = 0;
-        };
-
-
-        teArr.forEach((value) => {
-            teTotal = teTotal + value;
-        });
-        teRemaining = teBudget - teTotal - teSubtract
-
-        qbArr.forEach((value) => {
-            qbTotal = qbTotal + value;
-        });
-        qbRemaining = qbBudget - qbTotal - qbSubtract
-
-
-
-
-
-        $('#qbRemaining').text('$' + qbRemaining);
-        $('#rbRemaining').text('$' + rbRemaining)
-        $('#wrRemaining').text('$' + wrRemaining);
-        $('#teRemaining').text('$' + teRemaining);
-
-        let remainingTotal = budgetTotal - qbTotal - rbTotal - wrTotal - teTotal;
-
-        $('#budgetRemaining').text('$' + remainingTotal);
-
-        if (qbRemaining === 0) {
-            $('#qbRemaining').addClass('redText');
-        }
-        if (rbRemaining === 0) {
-            $('#rbRemaining').addClass('redText');
-        }
-        if (wrRemaining === 0) {
-            $('#wrRemaining').addClass('redText');
-        }
-        if (teRemaining === 0) {
-            $('#teRemaining').addClass('redText');
-        }
+    if (wrRemaining < 0) {
+        $('#wrRemaining').addClass('redText');
+    } else {
+        $('#wrRemaining').removeClass('redText');
     }
 
-    // adds drafted players/costs to arr to update rosters and budgets 
+    if (teRemaining < 0) {
+        $('#teRemaining').addClass('redText');
+    } else {
+        $('#teRemaining').removeClass('redText');
+    }
+}
 
-    addQB = () => {
-        if ($('#qbPrice').val().length === 0) {
-            return
-        } else {
-            qbArr.push(Number($('#qbPrice').val()));
-            $('#qbPrice').val('');
-            getRoster();
-            updateRemainingBudget();
-        }
-    };
+// Adds drafted players/costs to arr to update rosters and budgets 
+addQB = () => {
+    if ($('#qbPrice').val().length === 0) {
+        return
+    } else {
+        qbArr.push(Number($('#qbPrice').val()));
+        $('#qbPrice').val('');
+        getRoster();
+        updateBudgets();
+        renderPurchases();
+    }
+};
 
-    addRB = () => {
-        if ($('#rbPrice').val().length === 0) {
-            return
-        } else {
-            rbArr.push(Number($('#rbPrice').val()));
-            $('#rbPrice').val('');
-            getRoster();
-            updateRemainingBudget();
-        }
-    };
+addRB = () => {
+    if ($('#rbPrice').val().length === 0) {
+        return
+    } else {
+        rbArr.push(Number($('#rbPrice').val()));
+        $('#rbPrice').val('');
+        getRoster();
+        updateBudgets();
+        renderPurchases();
+    }
+};
 
-    addWR = () => {
-        if ($('#wrPrice').val().length === 0) {
-            return
-        } else {
-            wrArr.push(Number($('#wrPrice').val()));
-            $('#wrPrice').val('');
-            getRoster();
-            updateRemainingBudget();
-        }
-    };
+addWR = () => {
+    if ($('#wrPrice').val().length === 0) {
+        return
+    } else {
+        wrArr.push(Number($('#wrPrice').val()));
+        $('#wrPrice').val('');
+        getRoster();
+        updateBudgets();
+        renderPurchases();
+    }
+};
 
-    addTE = () => {
-        if ($('#tePrice').val().length === 0) {
-            return
-        } else {
-            teArr.push(Number($('#tePrice').val()));
-            $('#tePrice').val('');
-            getRoster();
-            updateRemainingBudget();
-        }
-    };
+addTE = () => {
+    if ($('#tePrice').val().length === 0) {
+        return
+    } else {
+        teArr.push(Number($('#tePrice').val()));
+        $('#tePrice').val('');
+        getRoster();
+        updateBudgets();
+        renderPurchases();
+    }
+};
 
+
+
+
+
+
+
+
+
+// updateRemainingBudget = () => {
+//     let qbTotal = 0;
+//     let rbTotal = 0;
+//     let wrTotal = 0;
+//     let teTotal = 0;
+
+//     rbArr.forEach((value) => {
+//         rbTotal = rbTotal + value;
+//     });
+//     rbRemaining = rbBudget - rbTotal
+//     // if (rbRemaining < 0) {
+//     //     rbOverspend = Math.abs(rbRemaining);
+//     //     rbRemaining = 0;
+//     //     qbSubtract = Math.ceil(rbOverspend / 2);
+//     //     teSubtract = rbOverspend - qbSubtract;
+//     // };
+
+//     wrArr.forEach((value) => {
+//         wrTotal = wrTotal + value;
+//     });
+//     wrRemaining = wrBudget - wrTotal
+//     // if (wrRemaining < 0) {
+//     //     wrOverspend = Math.abs(wrRemaining);
+//     //     wrRemaining = 0;
+//     //     qbSubtract = Math.ceil(wrOverspend / 2);
+//     //     teSubtract = wrOverspend - qbSubtract;
+//     // };
+
+//     teArr.forEach((value) => {
+//         teTotal = teTotal + value;
+//     });
+//     teRemaining = teBudget - teTotal
+
+//     qbArr.forEach((value) => {
+//         qbTotal = qbTotal + value;
+//     });
+//     qbRemaining = qbBudget - qbTotal
+// };
+
+
+
+
+
+
+
+
+
+
+// Renders player prices
+renderPurchases = () => {
+    $('#qbCost').text('');
+    $('#rbCost').text('');
+    $('#wrCost').text('');
+    $('#teCost').text('');
+
+    qbArr.map((value) => {
+        let index = qbArr.indexOf(value);
+        let li = $('<li>');
+        li.append('$' + value).attr('data-index', index).addClass('qbArr mb-2');
+        $('#qbCost').append(li);
+    });
+
+    rbArr.map((value) => {
+        let index = rbArr.indexOf(value);
+        let li = $('<li>');
+        li.append('$' + value).attr('data-index', index).addClass('rbArr mb-2');
+        $('#rbCost').append(li);
+    });
+
+    wrArr.map((value) => {
+        let index = wrArr.indexOf(value);
+        let li = $('<li>');
+        li.append('$' + value).attr('data-index', index).addClass('wrArr mb-2');
+        $('#wrCost').append(li);
+    });
+
+    teArr.map((value) => {
+        let index = teArr.indexOf(value);
+        let li = $('<li>');
+        li.append('$' + value).attr('data-index', index).addClass('teArr mb-2');
+        $('#teCost').append(li);
+    });
+};
+
+// DOM toggle
+$('#setup').click(() => {
+    $('#openSetup').toggle();
+    $('#closeSetup').toggle();
+    $('#setupDiv').toggle();
+});
+
+$('#draftTool').click(() => {
+    $('#openDraftTool').toggle();
+    $('#closeDraftTool').toggle();
+    $('#draftToolDiv').toggle();
+});
+
+$('#owned').click(() => {
+    $('#openPositionCost').toggle();
+    $('#closePositionCost').toggle();
+    $('#positionCostDiv').toggle();
+});
+
+$(document).on('click', '.qbArr', function () {
+    let index = $(this).attr('data-index');
+    qbArr.splice(index, 1);
+    renderPurchases();
     getRoster();
+    updateBudgets();
+});
 
-    // DOM toggle
+$(document).on('click', '.rbArr', function () {
+    let index = $(this).attr('data-index');
+    rbArr.splice(index, 1);
+    renderPurchases();
+    getRoster();
+    updateBudgets();
+});
 
-    $('#setup').click(() => {
-        $('#openSetup').toggle();
-        $('#closeSetup').toggle();
-        $('#setupDiv').toggle();
-    })
+$(document).on('click', '.wrArr', function () {
+    let index = $(this).attr('data-index');
+    wrArr.splice(index, 1);
+    renderPurchases();
+    getRoster();
+    updateBudgets();
+});
 
-    $('#draftTool').click(() => {
-        $('#openDraftTool').toggle();
-        $('#closeDraftTool').toggle();
-        $('#draftToolDiv').toggle();
-    })
-})
+$(document).on('click', '.teArr', function () {
+    let index = $(this).attr('data-index');
+    teArr.splice(index, 1);
+    renderPurchases();
+    getRoster();
+    updateBudgets();
+});
